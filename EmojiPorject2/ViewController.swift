dc9f87bc28ce68be281dac77debe6d7cd19b8663
Sokol8807 <sokol8807@gmail.com>
2022-05-26 14:04:00 +0300

    Initial Commit

---
+++ EmojiPorject2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmojiPorject2
+//
+//  Created by Ilya Sokolov on 26.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
