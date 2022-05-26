8b7fcffbafd677df9059a119dce2ff51872998cb
Sokol8807 <sokol8807@gmail.com>
2022-05-26 16:01:13 +0300

    сделай очередное задание

---
+++ EmojiPorject2/EmojiTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiPorject2
+//
+//  Created by Ilya Sokolov on 26.05.2022.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var nameLable: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+}
