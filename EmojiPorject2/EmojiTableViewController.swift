048301fda28cf90c1e78b4aee4e3d122d467d476
Sokol8807 <sokol8807@gmail.com>
2022-05-31 09:59:52 +0300

    добавил вторую таблицу - показ модально, прописал функцию добавления элементов в таблицу

--- EmojiPorject2/EmojiTableViewController.swift
+++ EmojiPorject2/EmojiTableViewController.swift
@@ -11,7 +11,7 @@ class EmojiTableViewController: UITableViewController {
 
     
     
-    let objects = [
+    var objects = [
     
         Emoji(emoji: "🥰", name: "Love", description: "Love is love", isFavourite: false),
         Emoji(emoji: "⚽️", name: "Football", description: "Let's play footbal together", isFavourite: false),
@@ -30,6 +30,22 @@ class EmojiTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
          self.navigationItem.leftBarButtonItem = self.editButtonItem
+    }
+    
+    
+    @IBAction func unwindSegue(segue: UIStoryboardSegue ) {
+        
+        guard segue.identifier == "saveSegue" else {return}
+        let sourceVC = segue.source as! NewEmojiTableViewController
+        let emoji = sourceVC.emoji
+        
+        let newIndexPath = IndexPath(row: objects.count, section: 0)
+        
+        objects.append(emoji)
+        
+        
+        tableView.insertRows(at: [newIndexPath], with: .fade)
+        
     }
 
     // MARK: - Table view data source
@@ -57,50 +73,71 @@ class EmojiTableViewController: UITableViewController {
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+            return .delete
+
+        }
+        
+
+    override func tableView(_ tableView: UITableView, commit editingStyle:UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath ) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            
+            objects.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
+        
+        }
+}
+    
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+        true
     }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+    
+        let movedEmoji = objects.remove(at: sourceIndexPath.row)
+        objects.insert(movedEmoji, at: destinationIndexPath.row)
+        tableView.reloadData()
+        
+        
     }
-    */
-
+    
+    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let done = doneAction(at: indexPath)
+        let favourite = favoriteAction(at: indexPath)
+        
+        return UISwipeActionsConfiguration(actions: [done, favourite])
+    }
+    
+    func doneAction(at indexPath: IndexPath) -> UIContextualAction {
+        
+        let action = UIContextualAction(style: .destructive, title: "Done") { (action, view, completion) in
+            
+            self.objects.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            completion(true)
+        }
+        
+        
+        action.backgroundColor = .systemGreen
+        action.image = UIImage(systemName: "checkmark.circle")
+        
+        return action
+        
+    }
+    func favoriteAction(at indexPath: IndexPath) -> UIContextualAction {
+        
+        var object = objects[indexPath.row]
+        let action = UIContextualAction(style: .normal, title: "Favourite") {(action, view, completion)
+            in
+            object.isFavourite = !object.isFavourite
+            self.objects[indexPath.row] = object
+            completion(true)
+            
+        }
+        action.backgroundColor = object.isFavourite ? .systemPurple : .systemGray
+        action.image = UIImage(systemName: "heart")
+        return action
+    }
 }
